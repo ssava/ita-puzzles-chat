915d7599c945b3acdb9e1851a79d31a5eb9b3862
Simone Sava <sasimone@live.com>
2019-10-14 00:44:10 +0200

    Added parsing support for LOGIN, JOIN, PART LOGOUT commands.

---
+++ ItaSoftware.Puzzles.Chat.Tests/CommandParserTests.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ItaSoftware.Puzzles.Chat.Tests
+{
+    [TestClass]
+    public class CommandParserTests
+    {
+        [TestMethod]
+        public void Server_responds_ERROR_to_unsupported_command()
+        {
+            CommandParser parser = new CommandParser();
+
+            string output = parser.Execute("BADCOMMAND\r\n");
+
+            Assert.AreEqual("ERROR Unsupported command\r\n", output);
+        }
+
+        [TestMethod]
+        public void Server_responds_OK_to_valid_LOGIN_command()
+        {
+            CommandParser parser = new CommandParser();
+
+            string output = parser.Execute("LOGIN user\r\n");
+
+            Assert.AreEqual("OK\r\n", output);
+        }
+
+        [TestMethod]
+        public void Server_responds_ERROR_to_LOGIN_command_wo_user()
+        {
+            CommandParser parser = new CommandParser();
+
+            string output = parser.Execute("LOGIN\r\n");
+
+            Assert.AreEqual("ERROR Need to specify a username.\r\n", output);
+        }
+
+        [TestMethod]
+        public void Server_responds_ERROR_to_LOGIN_command_with_trail_space()
+        {
+            CommandParser parser = new CommandParser();
+
+            string output = parser.Execute("LOGIN \r\n");
+
+            Assert.AreEqual("ERROR Need to specify a username.\r\n", output);
+        }
+
+        [TestMethod]
+        public void Server_responds_OK_to_JOIN_command()
+        {
+            CommandParser parser = new CommandParser();
+
+            string output = parser.Execute("JOIN #hello\r\n");
+
+            Assert.AreEqual("OK\r\n", output);
+        }
+
+        [TestMethod]
+        public void Server_responds_ERROR_to_JOIN_command_wo_room()
+        {
+            CommandParser parser = new CommandParser();
+
+            string output = parser.Execute("JOIN\r\n");
+
+            Assert.AreEqual("ERROR You need to specify a room to join.\r\n", output);
+        }
+
+        [TestMethod]
+        public void Server_responds_ERROR_to_JOIN_command_with_trail_space()
+        {
+            CommandParser parser = new CommandParser();
+
+            string output = parser.Execute("JOIN \r\n");
+
+            Assert.AreEqual("ERROR You need to specify a room to join.\r\n", output);
+        }
+
+        [TestMethod]
+        public void Server_responds_ERROR_to_JOIN_command_with_invalid_room_name()
+        {
+            CommandParser parser = new CommandParser();
+
+            string output = parser.Execute("JOIN invalidname\r\n");
+
+            Assert.AreEqual("ERROR Invalid room name.\r\n", output);
+        }
+
+
+        [TestMethod]
+        public void Server_responds_OK_to_PART_command()
+        {
+            CommandParser parser = new CommandParser();
+
+            string output = parser.Execute("PART #hello\r\n");
+
+            Assert.AreEqual("OK\r\n", output);
+        }
+
+        [TestMethod]
+        public void Server_responds_ERROR_to_PART_command_wo_room()
+        {
+            CommandParser parser = new CommandParser();
+
+            string output = parser.Execute("PART\r\n");
+
+            Assert.AreEqual("ERROR You need to specify a room to part.\r\n", output);
+        }
+
+        [TestMethod]
+        public void Server_responds_ERROR_to_PART_command_with_trail_space()
+        {
+            CommandParser parser = new CommandParser();
+
+            string output = parser.Execute("PART \r\n");
+
+            Assert.AreEqual("ERROR You need to specify a room to part.\r\n", output);
+        }
+
+        [TestMethod]
+        public void Server_responds_ERROR_to_PART_command_with_invalid_room_name()
+        {
+            CommandParser parser = new CommandParser();
+
+            string output = parser.Execute("PART invalidname\r\n");
+
+            Assert.AreEqual("ERROR Invalid room name.\r\n", output);
+        }
+    }
+}
---
+++ ItaSoftware.Puzzles.Chat/CommandParser.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ItaSoftware.Puzzles.Chat
+{
+    public class CommandParser
+    {
+        public const string CRLF = "\r\n";
+
+        public static IDictionary<string, ushort> ServMsg = new Dictionary<string, ushort>
+        {
+            { "OK", 0 },
+            { "ERROR", 1 },
+            { "GOTUSERMSG", 2 },
+            { "GOTROOMMSG", 3 }
+        };
+
+        public static IDictionary<string, ushort> Commands = new Dictionary<string, ushort>
+        {
+            { "LOGIN", 1 },
+            { "JOIN", 1 },
+            { "PART", 1 },
+            { "MSG", 2 },
+            { "GOTROOMMSG", 3 },
+            { "GOTUSERMSG", 2 },
+            { "LOGOUT", 0 }
+        };
+
+        public string Execute(string command)
+        {
+            string result = string.Empty;
+            string cmd_name = string.Empty;
+            string[] cmd_args = new string[0];
+            bool hasInvalidArgsCount = false;
+
+            command = command.Replace(CRLF, string.Empty).Trim();
+            cmd_name = command.Split(' ')[0];
+
+            /* Check if a command is supported */
+            if (!Commands.Keys.Contains(cmd_name))
+            {
+                result = "ERROR Unsupported command";
+                return string.Format("{0}{1}", result, CRLF);
+            }
+
+
+            /* Split whole command line */
+            cmd_args = command.Split(' ');
+
+            /* Check for correct arguments */
+            if ((cmd_args.Length - 1) < Commands[cmd_name])
+                result = "ERROR Invalid arguments number.";
+            else
+                cmd_args = cmd_args.Skip(1).ToArray();
+
+            /* Handle each commands */
+            hasInvalidArgsCount = result.StartsWith("ERROR");
+
+            switch (cmd_name)
+            {
+                case "LOGIN":
+                    if (hasInvalidArgsCount)
+                        result = "ERROR Need to specify a username.";
+                    else
+                        result = "OK";
+                    break;
+
+                case "JOIN":
+                    if (hasInvalidArgsCount)
+                        result = "ERROR You need to specify a room to join.";
+                    else
+                    { 
+                        if (!cmd_args[0].StartsWith("#"))
+                            result = "ERROR Invalid room name.";
+                        else
+                            result = "OK";
+                    }
+                    break;
+                case "PART":
+                    if (hasInvalidArgsCount)
+                        result = "ERROR You need to specify a room to part.";
+                    else
+                    {
+                        if (!cmd_args[0].StartsWith("#"))
+                            result = "ERROR Invalid room name.";
+                        else
+                            result = "OK";
+                    }
+                    break;
+            }
+
+            return string.Format("{0}{1}", result, CRLF);
+        }
+    }
+}
